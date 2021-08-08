10272699a43e512d931d7cbbcc164556cfa18530
Sergei Koroblev <rassafel@gmail.com>
2021-08-08 21:04:26 +0400

    Added repository to TagServiceImpl.

--- src/main/java/ru/samgtu/monolith/tag/service/impl/TagServiceImpl.java
+++ src/main/java/ru/samgtu/monolith/tag/service/impl/TagServiceImpl.java
@@ -2,13 +2,15 @@ package ru.samgtu.monolith.tag.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import ru.samgtu.monolith.tag.model.persistence.Tag;
+import ru.samgtu.monolith.tag.repository.TagRepository;
 import ru.samgtu.monolith.tag.service.TagService;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  * Creation date: 07.08.2021
@@ -20,24 +22,28 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 public class TagServiceImpl implements TagService {
+    private final TagRepository repository;
+
     @Override
     public List<Tag> getTags(Pageable pageable) {
-        ArrayList<Tag> list = new ArrayList<>();
-        list.add(getTagById(1L));
-        return list;
+        Page<Tag> tags = repository.findAll(pageable);
+        return tags.getContent();
     }
 
     @Override
     public List<Tag> getTagsByName(String name,
                                    Pageable pageable) {
-        return getTags(pageable);
+        Page<Tag> tags = repository.findByNameIgnoreCase(name, pageable);
+        return tags.getContent();
     }
 
     @Override
     public Tag getTagById(Long id) {
-        Tag tag = new Tag();
-        tag.setName("default Tag by id");
-        tag.setId(1L);
-        return tag;
+        return repository
+                .findById(id)
+                .orElseThrow(() -> {
+                    log.warn("Tag with id = {} does not exists", id);
+                    return new NoSuchElementException("Tag does not exists");
+                });
     }
 }
