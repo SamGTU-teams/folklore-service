e224a37258a8c89f3922092005da865e802d2dc1
Sergei Koroblev <rassafel@gmail.com>
2021-08-08 21:06:53 +0400

    Added JPA annotations.

--- src/main/java/ru/samgtu/monolith/tag/model/persistence/Tag.java
+++ src/main/java/ru/samgtu/monolith/tag/model/persistence/Tag.java
@@ -2,19 +2,28 @@ package ru.samgtu.monolith.tag.model.persistence;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 /**
  * Creation date: 07.08.2021
  *
  * @author rassafel
  * @version 1.0
  */
+@Entity
+@Table(name = "tag")
 @RequiredArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    @EqualsAndHashCode.Include
     private Long id;
 
+    @Column(name = "name", nullable = false, length = 20, unique = true)
     private String name;
 }
