bd3496ec51a97c712eebe4637b1097198398355c
Sergei Koroblev <rassafel@gmail.com>
2021-08-09 17:02:46 +0400

    Added separators for MimeObjects.

--- src/main/java/ru/samgtu/monolith/config/OrikaMapper.java
+++ src/main/java/ru/samgtu/monolith/config/OrikaMapper.java
@@ -2,6 +2,7 @@ package ru.samgtu.monolith.config;
 
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.impl.ConfigurableMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import ru.samgtu.monolith.folklore.model.dto.BuildingDto;
 import ru.samgtu.monolith.folklore.model.persistence.Building;
@@ -16,6 +17,12 @@ import ru.samgtu.monolith.tag.model.persistence.Tag;
  */
 @Component
 public class OrikaMapper extends ConfigurableMapper {
+    @Value("${media.separator:\\s+N\\s+}")
+    private String mediaSeparator;
+
+    @Value("${media.type.separator:\\s+}")
+    private String mediaTypeSeparator;
+
     @Override
     protected void configure(MapperFactory factory) {
         factory.classMap(Tag.class, TagDto.class)
