5582071a6a6d5bcd1a5071f421e101a329d59f21
Sergei Koroblev <rassafel@gmail.com>
2021-08-12 13:27:50 +0400

    Added plug.

--- src/main/java/ru/samgtu/monolith/activity/controller/impl/ActivityControllerImpl.java
+++ src/main/java/ru/samgtu/monolith/activity/controller/impl/ActivityControllerImpl.java
@@ -3,18 +3,14 @@ package ru.samgtu.monolith.activity.controller.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RestController;
 import ru.samgtu.monolith.activity.controller.ActivityController;
 import ru.samgtu.monolith.activity.model.dto.ActivityDto;
 import ru.samgtu.monolith.activity.model.dto.ScheduledActivityDto;
-import ru.samgtu.monolith.activity.model.persistence.Activity;
 import ru.samgtu.monolith.activity.service.ActivityService;
 import ru.samgtu.monolith.tag.model.dto.TagDto;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Set;
 
 /**
@@ -37,7 +33,7 @@ public class ActivityControllerImpl implements ActivityController {
 
     @Override
     public Page<ActivityDto> getActivitiesByParams(String name, LocalDateTime from, LocalDateTime to, int page, int size) {
-
+        return null;
     }
 
     @Override
