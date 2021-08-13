ac9046885a72e9690ceca0be23f9f2c06bc36b1e
Sergei Koroblev <rassafel@gmail.com>
2021-08-13 23:07:16 +0400

    Created ActivityInfo.

---
+++ frontend/src/views/ActivityInfo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="activivty">Activity {{ id }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: "ActivityInfo",
+  props: ["id"],
+})
+</script>
