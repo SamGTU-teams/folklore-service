7e77f1df452a44305c4791effe86cc85b7dee593
Sergei Koroblev <rassafel@gmail.com>
2021-08-14 03:15:35 +0400

    Removed CORS configuration.

--- backend/src/main/java/ru/samgtu/monolith/config/WebMvcConfiguration.java
+++ backend/src/main/java/ru/samgtu/monolith/config/WebMvcConfiguration.java
@@ -2,19 +2,11 @@ package ru.samgtu.monolith.config;
 
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
-import java.util.Collections;
 
 /**
  * Creation date: 13.08.2021
@@ -24,33 +16,6 @@ import java.util.Collections;
  */
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
-    @Bean
-    public FilterRegistrationBean<CorsFilter> filterRegistrationBean() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-
-        corsConfiguration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:8080",
-                "http://localhost:8081",
-                "https://rassafel-folklore-service.herokuapp.com/"));
-
-        corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
-
-        corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
-
-        corsConfiguration.setAllowCredentials(true);
-
-        corsConfiguration.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
-        corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-
-        CorsFilter corsFilter = new CorsFilter(corsConfigurationSource);
-
-        FilterRegistrationBean<CorsFilter> filterRegistrationBean = new FilterRegistrationBean<>(corsFilter);
-        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return filterRegistrationBean;
-    }
-
     @Bean
     public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
         return container -> container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/"));
