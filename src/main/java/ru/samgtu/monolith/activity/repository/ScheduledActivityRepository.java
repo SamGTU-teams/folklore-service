7aa6b89c0be92ad278c596237d0055cc92155cfe
Sergei Koroblev <rassafel@gmail.com>
2021-08-14 17:51:14 +0400

    Created query.

--- src/main/java/ru/samgtu/monolith/activity/repository/ScheduledActivityRepository.java
+++ src/main/java/ru/samgtu/monolith/activity/repository/ScheduledActivityRepository.java
@@ -1,10 +1,13 @@
 package ru.samgtu.monolith.activity.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.samgtu.monolith.activity.model.persistence.ScheduledActivity;
 import ru.samgtu.monolith.activity.model.persistence.ScheduledId;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -16,4 +19,11 @@ import java.util.List;
 public interface ScheduledActivityRepository extends JpaRepository<ScheduledActivity, ScheduledId> {
     @Query("select sa.id from ScheduledActivity sa")
     List<ScheduledId> findAllIds();
+
+    @Query("select s.id.activityId, min(s.id.dateTime) as dt " +
+            "from ScheduledActivity s " +
+            "where s.id.dateTime >= ?1 " +
+            "group by s.id.activityId " +
+            "order by dt")
+    Page<Object[]> findMinDateTimeIsGreaterThanEqualOrderByIdDateTimeAsc(LocalDateTime dateTime, Pageable pageable);
 }
