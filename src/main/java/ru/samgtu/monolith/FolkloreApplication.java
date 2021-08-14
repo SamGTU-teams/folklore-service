0aa442949e3cc26b5810f59ea6f295b3019005a3
Sergei Koroblev <rassafel@gmail.com>
2021-08-14 14:50:57 +0400

    Enabled scheduling.

--- src/main/java/ru/samgtu/monolith/FolkloreApplication.java
+++ src/main/java/ru/samgtu/monolith/FolkloreApplication.java
@@ -2,6 +2,7 @@ package ru.samgtu.monolith;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Creation date: 08.08.2021
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version 1.0
  */
 @SpringBootApplication
+@EnableScheduling
 public class FolkloreApplication {
     public static void main(String[] args) {
         SpringApplication.run(FolkloreApplication.class, args);
