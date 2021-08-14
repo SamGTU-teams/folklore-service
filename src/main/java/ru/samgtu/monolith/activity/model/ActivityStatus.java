98cefd771a345f378bd0315c90bbf3149a0a936e
Sergei Koroblev <rassafel@gmail.com>
2021-08-14 13:37:16 +0400

    Created slices.

--- src/main/java/ru/samgtu/monolith/activity/model/ActivityStatus.java
+++ src/main/java/ru/samgtu/monolith/activity/model/ActivityStatus.java
@@ -1,5 +1,9 @@
 package ru.samgtu.monolith.activity.model;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 /**
  * Creation date: 08.08.2021
  *
@@ -9,5 +13,41 @@ package ru.samgtu.monolith.activity.model;
 public enum ActivityStatus {
     SCHEDULED,
     STARTED,
-    ENDED
+    ENDED;
+
+    public static ActivityStatus[] getBefore(ActivityStatus value) {
+        return getBeforeStream(value)
+                .sorted(Comparator.comparingInt(Enum::ordinal))
+                .toArray(ActivityStatus[]::new);
+    }
+
+    public static ActivityStatus[] getBeforeOrEquals(ActivityStatus value) {
+        return Stream.concat(getBeforeStream(value),
+                        Stream.of(value))
+                .sorted(Comparator.comparingInt(Enum::ordinal))
+                .toArray(ActivityStatus[]::new);
+    }
+
+    public static ActivityStatus[] getAfter(ActivityStatus value) {
+        return getAfterStream(value)
+                .sorted(Comparator.comparingInt(Enum::ordinal))
+                .toArray(ActivityStatus[]::new);
+    }
+
+    public static ActivityStatus[] getAfterOrEquals(ActivityStatus value) {
+        return Stream.concat(getAfterStream(value),
+                        Stream.of(value))
+                .sorted(Comparator.comparingInt(Enum::ordinal))
+                .toArray(ActivityStatus[]::new);
+    }
+
+    private static Stream<ActivityStatus> getBeforeStream(ActivityStatus value) {
+        return Arrays.stream(ActivityStatus.values())
+                .filter(status -> status.ordinal() > value.ordinal());
+    }
+
+    private static Stream<ActivityStatus> getAfterStream(ActivityStatus value) {
+        return Arrays.stream(ActivityStatus.values())
+                .filter(status -> status.ordinal() < value.ordinal());
+    }
 }
