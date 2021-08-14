63181b8bc38f1ccec30dd7a6ef3b7e0a7711e930
Sergei Koroblev <rassafel@gmail.com>
2021-08-14 13:55:46 +0400

    Fixed test.

--- src/test/java/ru/samgtu/monolith/activity/service/impl/ActivityTest.java
+++ src/test/java/ru/samgtu/monolith/activity/service/impl/ActivityTest.java
@@ -3,6 +3,7 @@ package ru.samgtu.monolith.activity.service.impl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import ru.samgtu.monolith.activity.model.persistence.Activity;
@@ -10,7 +11,6 @@ import ru.samgtu.monolith.activity.service.ActivityService;
 
 import javax.persistence.EntityManager;
 import java.time.LocalDateTime;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -29,8 +29,8 @@ public class ActivityTest {
         LocalDateTime ld = LocalDateTime.parse("2010-10-10T12:00");
         System.out.println();
         PageRequest pageRequest = PageRequest.of(2, 1);
-        List<Activity> result = activityService.findByDateAfterThan(LocalDateTime.now(), pageRequest);
-        assertEquals(result.size(), 2);
+        Page<Activity> result = activityService.findByDateAfterThan(LocalDateTime.now(), pageRequest);
+        assertEquals(result.getNumberOfElements(), 2);
     }
 
 }
