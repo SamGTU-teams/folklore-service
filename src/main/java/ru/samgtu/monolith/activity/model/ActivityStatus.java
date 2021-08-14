39136e6dfe70c877eb9838bc55488c42fed23f5b
Sergei Koroblev <rassafel@gmail.com>
2021-08-14 14:21:34 +0400

    Polish and fix code.

--- src/main/java/ru/samgtu/monolith/activity/model/ActivityStatus.java
+++ src/main/java/ru/samgtu/monolith/activity/model/ActivityStatus.java
@@ -16,38 +16,33 @@ public enum ActivityStatus {
     ENDED;
 
     public static ActivityStatus[] getBefore(ActivityStatus value) {
-        return getBeforeStream(value)
-                .sorted(Comparator.comparingInt(Enum::ordinal))
-                .toArray(ActivityStatus[]::new);
+        return toArray(getBeforeStream(value));
     }
 
     public static ActivityStatus[] getBeforeOrEquals(ActivityStatus value) {
-        return Stream.concat(getBeforeStream(value),
-                        Stream.of(value))
-                .sorted(Comparator.comparingInt(Enum::ordinal))
-                .toArray(ActivityStatus[]::new);
+        return toArray(Stream.concat(getBeforeStream(value), Stream.of(value)));
     }
 
     public static ActivityStatus[] getAfter(ActivityStatus value) {
-        return getAfterStream(value)
-                .sorted(Comparator.comparingInt(Enum::ordinal))
-                .toArray(ActivityStatus[]::new);
+        return toArray(getAfterStream(value));
     }
 
     public static ActivityStatus[] getAfterOrEquals(ActivityStatus value) {
-        return Stream.concat(getAfterStream(value),
-                        Stream.of(value))
-                .sorted(Comparator.comparingInt(Enum::ordinal))
+        return toArray(Stream.concat(getAfterStream(value), Stream.of(value)));
+    }
+
+    private static ActivityStatus[] toArray(Stream<ActivityStatus> stream) {
+        return stream.sorted(Comparator.comparingInt(Enum::ordinal))
                 .toArray(ActivityStatus[]::new);
     }
 
     private static Stream<ActivityStatus> getBeforeStream(ActivityStatus value) {
         return Arrays.stream(ActivityStatus.values())
-                .filter(status -> status.ordinal() > value.ordinal());
+                .filter(status -> status.ordinal() < value.ordinal());
     }
 
     private static Stream<ActivityStatus> getAfterStream(ActivityStatus value) {
         return Arrays.stream(ActivityStatus.values())
-                .filter(status -> status.ordinal() < value.ordinal());
+                .filter(status -> status.ordinal() > value.ordinal());
     }
 }
