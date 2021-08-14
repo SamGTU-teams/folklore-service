f09ff80ff46d780b1eb448d8216671ebf850fd1a
Sergei Koroblev <rassafel@gmail.com>
2021-08-14 17:52:21 +0400

    Created method findByDateAfterThan.

--- src/main/java/ru/samgtu/monolith/activity/service/ScheduledActivityService.java
+++ src/main/java/ru/samgtu/monolith/activity/service/ScheduledActivityService.java
@@ -1,5 +1,11 @@
 package ru.samgtu.monolith.activity.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import ru.samgtu.monolith.activity.model.persistence.ScheduledActivity;
+
+import java.time.LocalDateTime;
+
 /**
  * Creation date: 14.08.2021
  *
@@ -7,4 +13,5 @@ package ru.samgtu.monolith.activity.service;
  * @version 1.0
  */
 public interface ScheduledActivityService {
+    Page<ScheduledActivity> findByDateAfterThan(LocalDateTime from, Pageable pageable);
 }
--- src/main/java/ru/samgtu/monolith/activity/service/impl/ScheduledActivityServiceImpl.java
+++ src/main/java/ru/samgtu/monolith/activity/service/impl/ScheduledActivityServiceImpl.java
@@ -2,18 +2,27 @@ package ru.samgtu.monolith.activity.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.samgtu.monolith.activity.model.ActivityStatus;
+import ru.samgtu.monolith.activity.model.persistence.Activity;
 import ru.samgtu.monolith.activity.model.persistence.ScheduledActivity;
 import ru.samgtu.monolith.activity.model.persistence.ScheduledId;
+import ru.samgtu.monolith.activity.repository.ActivityRepository;
 import ru.samgtu.monolith.activity.repository.ScheduledActivityRepository;
 import ru.samgtu.monolith.activity.service.ScheduledActivityService;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Creation date: 14.08.2021
@@ -25,31 +34,69 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Slf4j
 public class ScheduledActivityServiceImpl implements ScheduledActivityService {
-    private final ScheduledActivityRepository repository;
+    private final ScheduledActivityRepository scheduledActivityRepository;
+
+    private final ActivityRepository activityRepository;
 
     @Transactional
     @Scheduled(fixedDelayString = "${activity.status.rate}")
     public void checkStatuses() {
         log.info("Start check statuses");
         LocalDateTime now = LocalDateTime.now();
-        for (ScheduledId id : repository.findAllIds()) {
-            Optional<ScheduledActivity> optionalScheduledActivity = repository.findById(id);
+        for (ScheduledId id : scheduledActivityRepository.findAllIds()) {
+            Optional<ScheduledActivity> optionalScheduledActivity = scheduledActivityRepository.findById(id);
             if (!optionalScheduledActivity.isPresent()) {
                 continue;
             }
             ScheduledActivity scheduledActivity = optionalScheduledActivity.get();
             LocalDateTime dateTime = scheduledActivity.getId().getDateTime();
             Duration duration = scheduledActivity.getActivity().getDuration();
-            LocalDateTime endDateTime;
-            if (now.compareTo(dateTime) < 1) {
-                setStatus(scheduledActivity, ActivityStatus.SCHEDULED);
-            } else if (now.compareTo(endDateTime = dateTime.plus(duration)) < 1) {
-                setStatus(scheduledActivity, ActivityStatus.STARTED);
-            } else {
-                setStatus(scheduledActivity, ActivityStatus.ENDED);
-            }
+            setStatus(scheduledActivity, calcStatus(now, dateTime, duration));
         }
         log.info("Finish check statuses");
+    }
+
+    @Override
+    public Page<ScheduledActivity> findByDateAfterThan(LocalDateTime from, Pageable pageable) {
+        // ToDo: create cache table
+        pageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize());
+
+        Page<Object[]> page = scheduledActivityRepository.findMinDateTimeIsGreaterThanEqualOrderByIdDateTimeAsc(from, pageable);
+        Map<Long, LocalDateTime> map = page.stream()
+                .collect(Collectors.toMap(obj -> (Long) obj[0], obj -> (LocalDateTime) obj[1]));
+
+        List<Activity> activities = activityRepository.findAllById(map.keySet());
+
+        LocalDateTime now = LocalDateTime.now();
+        List<ScheduledActivity> scheduledActivities =
+                activities.stream().map(activity -> {
+                    Long activityId = activity.getId();
+                    LocalDateTime dateTime = map.get(activityId);
+                    Duration duration = activity.getDuration();
+
+                    ScheduledId id = new ScheduledId();
+                    id.setActivityId(activityId);
+                    id.setDateTime(dateTime);
+
+                    ScheduledActivity scheduledActivity = new ScheduledActivity();
+                    ActivityStatus status = calcStatus(now, dateTime, duration);
+                    scheduledActivity.setActivity(activity);
+                    scheduledActivity.setStatus(status);
+
+                    return scheduledActivity;
+                }).collect(Collectors.toList());
+
+        return new PageImpl<>(scheduledActivities, pageable, page.getTotalElements());
+    }
+
+    private ActivityStatus calcStatus(LocalDateTime now, LocalDateTime dateTime, Duration duration) {
+        if (now.compareTo(dateTime) < 1) {
+            return ActivityStatus.SCHEDULED;
+        } else if (now.compareTo(dateTime.plus(duration)) < 1) {
+            return ActivityStatus.STARTED;
+        } else {
+            return ActivityStatus.ENDED;
+        }
     }
 
     private void setStatus(ScheduledActivity scheduledActivity, ActivityStatus status) {
@@ -60,6 +107,6 @@ public class ScheduledActivityServiceImpl implements ScheduledActivityService {
         log.debug("Changing the status of the object with id = {} from {} to {} ",
                 scheduledActivity.getId(), currentStatus, status);
         scheduledActivity.setStatus(status);
-        repository.save(scheduledActivity);
+        scheduledActivityRepository.save(scheduledActivity);
     }
 }
