780d6e3b89a4f85c4df44613433c8f078b07b066
Sergei Koroblev <rassafel@gmail.com>
2021-08-15 23:30:07 +0400

    Optimized imports.

--- backend/src/main/java/ru/samgtu/monolith/config/SwaggerConfiguration.java
+++ backend/src/main/java/ru/samgtu/monolith/config/SwaggerConfiguration.java
@@ -3,13 +3,10 @@ package ru.samgtu.monolith.config;
 import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
