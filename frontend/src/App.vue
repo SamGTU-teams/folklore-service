89fcf73fd2d7e6d2c51a5d25196153ac69245eac
Pavel Makeev <profi_makeev@mail.ru>
2021-08-15 21:54:00 +0400

    clean code format done

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,33 +5,49 @@
         <div id="Logo">
           LOGO
         </div>
-        <div id="MenuPuncts">                  
-          <router-link to="/login"><div class="MenuElement">Войти</div></router-link>
-          <router-link to="/profile"><div class="MenuElement">Профиль</div></router-link>
-          <router-link to="/afisha"><div class="MenuElement">Афиша</div></router-link>
-          <router-link to="/"><div class="MenuElement">Главная</div></router-link> 
+        <div id="MenuPuncts">
+          <router-link to="/login"
+            ><div class="MenuElement">Войти</div></router-link
+          >
+          <router-link to="/profile"
+            ><div class="MenuElement">Профиль</div></router-link
+          >
+          <router-link to="/afisha"
+            ><div class="MenuElement">Афиша</div></router-link
+          >
+          <router-link to="/"
+            ><div class="MenuElement">Главная</div></router-link
+          >
         </div>
       </div>
       <div class="row show-on-medium hide-on-med-and-up" id="MenuGrid">
         <div class="col s12 ">LOGO</div>
-        <router-link to="/"><div class="col offset-m4 m4 s12">Главная</div></router-link> 
-        <router-link to="/afisha"><div class="col offset-m4 m4 s12">Афиша</div></router-link> 
-        <router-link to="/profile"><div class="col offset-m4 m4 s12">Профиль</div></router-link> 
-        <router-link to="/login"><div class="col offset-m4 m4 s12">Войти</div></router-link> 
+        <router-link to="/"
+          ><div class="col offset-m4 m4 s12">Главная</div></router-link
+        >
+        <router-link to="/afisha"
+          ><div class="col offset-m4 m4 s12">Афиша</div></router-link
+        >
+        <router-link to="/profile"
+          ><div class="col offset-m4 m4 s12">Профиль</div></router-link
+        >
+        <router-link to="/login"
+          ><div class="col offset-m4 m4 s12">Войти</div></router-link
+        >
       </div>
     </div>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 <script>
-  import { defineComponent } from 'vue'
-  import M from 'materialize-css'
-  export default defineComponent({
-    name: "App",
-    mounted () {
-      M.AutoInit()
-    },
-  })
+import { defineComponent } from "vue";
+import M from "materialize-css";
+export default defineComponent({
+  name: "App",
+  mounted() {
+    M.AutoInit();
+  },
+});
 </script>
 <style>
 #app {
@@ -43,38 +59,39 @@
 #nav {
   margin-top: 30px;
 }
-.router-link-exact-active>div,.router-link-active>div{
+.router-link-exact-active > div,
+.router-link-active > div {
   border-radius: 15px;
-  background-color: #FFF;
-  color: #51495C;
+  background-color: #fff;
+  color: #2b3f8d;
 }
-.row{
-    font-size: 18px;
-    color: #FFF;
-  }
-#Logo{
-    width:35%;
-    float: left;
-    text-align: left;
-  }
-#MenuPuncts{
-    width: 65%;
-    float: right;
-  }
-.MenuElement{
-    color: #FFF;
-    margin-left: 15px;
-    padding: 0px 15px;
-    float: right;
-  }
-#MenuGrid{
-    color: #FFF;
-  }
-.col{
-    height: 100%;
-    color: #FFF;
-  }
-body{
-    background-color: #444444;
-    }
+.row {
+  font-size: 18px;
+  color: #fff;
+}
+#Logo {
+  width: 35%;
+  float: left;
+  text-align: left;
+}
+#MenuPuncts {
+  width: 65%;
+  float: right;
+}
+.MenuElement {
+  color: #fff;
+  margin-left: 15px;
+  padding: 0px 15px;
+  float: right;
+}
+#MenuGrid {
+  color: #fff;
+}
+.col {
+  height: 100%;
+  color: #fff;
+}
+body {
+  background-color: #2b3f8d;
+}
 </style>
