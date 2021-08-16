ad466332eaaaa185677d4e147d1d8065ef6aaf27
Sergei Koroblev <rassafel@gmail.com>
2021-08-16 21:58:00 +0400

    Updated repository.

--- backend/src/main/java/ru/samgtu/monolith/place/repository/PlaceRepository.java
+++ backend/src/main/java/ru/samgtu/monolith/place/repository/PlaceRepository.java
@@ -2,11 +2,12 @@ package ru.samgtu.monolith.place.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.samgtu.monolith.place.model.persistence.Place;
-import ru.samgtu.monolith.tag.model.persistence.Tag;
 
 import java.util.Collection;
+import java.util.Optional;
 
 /**
  * Creation date: 08.08.2021
@@ -17,5 +18,8 @@ import java.util.Collection;
 public interface PlaceRepository extends JpaRepository<Place, Long> {
     Page<Place> findByNameStartsWithIgnoreCase(String name, Pageable pageable);
 
-    Page<Place> findByTagsIn(Collection<Tag> tags, Pageable pageable);
+    Page<Place> findByTagsIdIn(Collection<String> ids, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"lob"})
+    Optional<Place> findInfoById(Long id);
 }
