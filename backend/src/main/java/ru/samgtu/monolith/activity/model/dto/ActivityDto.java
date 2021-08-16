2cfd5390859aebccf878ef004d47be9c9879bc32
Sergei Koroblev <rassafel@gmail.com>
2021-08-17 01:39:40 +0400

    Added point field.

--- backend/src/main/java/ru/samgtu/monolith/activity/model/dto/ActivityDto.java
+++ backend/src/main/java/ru/samgtu/monolith/activity/model/dto/ActivityDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonView;
 import io.swagger.annotations.ApiModel;
 import lombok.Data;
 import ru.samgtu.monolith.config.JacksonViews;
+import ru.samgtu.monolith.model.Point;
 import ru.samgtu.monolith.place.model.dto.PlaceDto;
 import ru.samgtu.monolith.tag.model.dto.TagDto;
 
@@ -26,10 +27,7 @@ public class ActivityDto {
     private String name;
 
     @JsonView(JacksonViews.DataWithoutLob.class)
-    private Double lat;
-
-    @JsonView(JacksonViews.DataWithoutLob.class)
-    private Double lon;
+    private Point point;
 
     @JsonView(JacksonViews.DataWithoutLob.class)
     private List<TagDto> tags;
