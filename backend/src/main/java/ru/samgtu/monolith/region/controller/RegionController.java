4f77da1e04b07d890fb80e3bdf75b7324658b76b
Sergei Koroblev <rassafel@gmail.com>
2021-08-16 22:42:27 +0400

    Optimized imports.

--- backend/src/main/java/ru/samgtu/monolith/region/controller/RegionController.java
+++ backend/src/main/java/ru/samgtu/monolith/region/controller/RegionController.java
@@ -4,7 +4,10 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.samgtu.monolith.model.ExceptionInfo;
 import ru.samgtu.monolith.region.model.dto.RegionDto;
 
