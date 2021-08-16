7449722f0a094cbf9acd2255f7fb61a0273fb347
Pavel Makeev <profi_makeev@mail.ru>
2021-08-16 17:33:29 +0400

    edited cards content

--- frontend/src/views/Afisha.vue
+++ frontend/src/views/Afisha.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="container">
     <div class="row">
+      <div class="col s12 m12 l12 titleRow">
+        Экскурсии по Новгороду
+      </div>
+    </div>
+    <div class="row">
       <div
         class="col s12 offset-m3 m6 l4"
         v-for="card in cards"
@@ -8,7 +13,8 @@
       >
         <afisha-card
           v-bind:imgUrl="card.imgUrl"
-          v-bind:textContent="card.textContent"
+          v-bind:titleText="card.titleText"
+          v-bind:subtitleText="card.subtitleText"
         />
       </div>
     </div>
@@ -31,75 +37,18 @@ export default defineComponent({
       cards: [
         {
           imgUrl: `https://picsum.photos/1920/1080`,
-          textContent: `I am a very simple card. I am good at containing small bits of
-              information. I am convenient because I require little markup to
-              use effectively.`,
+          titleText: `Экскурсия в Новгородский кремль`,
+          subtitleText: `8, Новгородский кремль, Великий Новгород`,
         },
         {
-          imgUrl: `https://picsum.photos/3840/2160`,
-          textContent: `I am a very simple card. I am good at containing small bits of
-              information. I am convenient because I require little markup to
-              use effectively.`,
+          imgUrl: `https://picsum.photos/1920/1080`,
+          titleText: `Экскурсия в Софийский собор`,
+          subtitleText: `11, Новгородский кремль, Великий Новгород, Россия`,
         },
         {
-          imgUrl: `https://picsum.photos/1024/720`,
-          textContent: `I am a very simple card. I am good at containing small bits of
-              information. I am convenient because I require little markup to
-              use effectively.`,
-        },
-        {
-          imgUrl: `https://picsum.photos/3840/2160`,
-          textContent: `I am a very simple card. I am good at containing small bits of
-              information. I am convenient because I require little markup to
-              use effectively.`,
-        },
-        {
-          imgUrl: `https://picsum.photos/1024/720`,
-          textContent: `I am a very simple card. I am good at containing small bits of
-              information. I am convenient because I require little markup to
-              use effectively.`,
-        },
-        {
-          imgUrl: `https://picsum.photos/3840/2160`,
-          textContent: `I am a very simple card. I am good at containing small bits of
-              information. I am convenient because I require little markup to
-              use effectively.`,
-        },
-        {
-          imgUrl: `https://picsum.photos/1024/720`,
-          textContent: `I am a very simple card. I am good at containing small bits of
-              information. I am convenient because I require little markup to
-              use effectively.`,
-        },
-        {
-          imgUrl: `https://picsum.photos/3840/2160`,
-          textContent: `I am a very simple card. I am good at containing small bits of
-              information. I am convenient because I require little markup to
-              use effectively.`,
-        },
-        {
-          imgUrl: `https://picsum.photos/1024/720`,
-          textContent: `I am a very simple card. I am good at containing small bits of
-              information. I am convenient because I require little markup to
-              use effectively.`,
-        },
-        {
-          imgUrl: `https://picsum.photos/3840/2160`,
-          textContent: `I am a very simple card. I am good at containing small bits of
-              information. I am convenient because I require little markup to
-              use effectively.`,
-        },
-        {
-          imgUrl: `https://picsum.photos/1024/720`,
-          textContent: `I am a very simple card. I am good at containing small bits of
-              information. I am convenient because I require little markup to
-              use effectively.`,
-        },
-        {
-          imgUrl: `https://picsum.photos/3840/2160`,
-          textContent: `I am a very simple card. I am good at containing small bits of
-              information. I am convenient because I require little markup to
-              use effectively.`,
+          imgUrl: `https://picsum.photos/1920/1080`,
+          titleText: `День в мужском монастыре Свято-Юрьев`,
+          subtitleText: `Юрьевское ш., 10, Великий Новгород`,
         },
       ],
     };
@@ -107,7 +56,8 @@ export default defineComponent({
 });
 </script>
 <style>
-.container {
-  padding-top: 20px;
+.titleRow {
+  text-align: left;
+  font-size: 1em;
 }
 </style>
