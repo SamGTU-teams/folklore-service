daeadcfe44f25727fda0bca61467a6fefb7dabad
Sergei Koroblev <rassafel@gmail.com>
2021-08-17 19:21:29 +0400

    Fixed query.

--- backend/src/main/java/ru/samgtu/monolith/activity/repository/ActivityRepository.java
+++ backend/src/main/java/ru/samgtu/monolith/activity/repository/ActivityRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.samgtu.monolith.activity.model.persistence.Activity;
 import ru.samgtu.monolith.tag.model.persistence.Tag;
 
@@ -21,4 +22,27 @@ public interface ActivityRepository extends JpaRepository<Activity, Long> {
 
     @EntityGraph(attributePaths = "lob")
     Optional<Activity> findInfoById(Long id);
+
+    @Query(value = "SELECT p.* " +
+            "FROM place p " +
+            "RIGHT JOIN ( " +
+            "SELECT pt.place_id " +
+            "FROM place_tag pt " +
+            "WHERE pt.tag_id SIMILAR TO ?1 " +
+            "GROUP BY pt.place_id " +
+            "HAVING COUNT(pt.tag_id) > 0 " +
+            ") t " +
+            "ON p.id = t.place_id " +
+            "\\n– #pageable\\n",
+            countQuery = "SELECT COUNT(*) " +
+                    "FROM activity a " +
+                    "RIGHT JOIN ( " +
+                    "SELECT at.activity_id " +
+                    "FROM activity_tag at " +
+                    "WHERE at.tag_id SIMILAR TO ?1 " +
+                    "GROUP BY at.activity_id " +
+                    "HAVING COUNT(at.tag_id) > 0 " +
+                    ") t",
+            nativeQuery = true)
+    Page<Activity> findByTagIdRegex(String regex, Pageable pageable);
 }
