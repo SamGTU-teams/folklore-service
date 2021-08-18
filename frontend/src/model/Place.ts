f5ded9c768899be26c05dba6b35efaea4d0fa2aa
Sergei Koroblev <rassafel@gmail.com>
2021-08-18 22:12:22 +0400

    Updated Activity and Place: - Changed from interface to class; - Extends MainObject;

--- frontend/src/model/Place.ts
+++ frontend/src/model/Place.ts
@@ -2,4 +2,4 @@ import { Tag } from "@/model/Tag";
 import { Point } from "@/model/Point";
 import { MainObject } from "@/model/MainObject";
 
-export interface Place extends MainObject {}
+export class Place extends MainObject {}
