ba69459562dde76e1cceee57813272aac5d77926
Pavel Makeev <profi_makeev@mail.ru>
2021-08-19 19:06:52 +0400

    fixed dropup-card style

--- frontend/src/components/GisMap.vue
+++ frontend/src/components/GisMap.vue
@@ -50,13 +50,13 @@ export default defineComponent({
         });
         var inPopap = `<div class='Popup'>
                           <div class="lable-container">
-                            <img class="img"style src='${place.imageUrl}' width="100%"><br>
-                            <div class="place-name">${place.name}<br></div>
-                            <div class="place-address">${place.address}<br></div>                         
+                            <img class="img" style src='${place.imageUrl}' width="100%">
+                            <div class="place-name">${place.name}</div>
+                            <div class="place-address">${place.address}</div>                         
                             <a href="/place/${place.id}">
-                              <div class="button">
-                                Подробнее                            
-                              </div>
+                                <div class="button">
+                                Подробнее                         
+                                </div>
                             </a>
                           </div>
                         </div>`;
@@ -78,40 +78,56 @@ export default defineComponent({
 }
 .Popup {
   width: 150px;
+  height: 245px;
   text-align: center;
 }
-.img{
+.img {
   width: 100%;
   height: 100%;
-  margin: calc(7px + (2.5 - 7) * ((100vw - 500px) / (1920 - 500))) 0;
+  margin: 7px 0px;
+  /* margin: calc(7px + (2.5 - 7) * ((100vw - 500px) / (1920 - 500))) 0; */
 }
 .place-name,
 .place-address {
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
 }
 
-.lable-container{
+.lable-container {
   margin: calc(10px + (15 - 5) * ((100vw - 500px) / (1920 - 500))) 0;
 }
 
 .place-name {
-  font-size: calc(20px + (16 - 20) * ((100vw - 500px) / (1920 - 500)));
-  padding-bottom: calc(7px + (2.5 - 7) * ((100vw - 500px) / (1920 - 500)));
+  height: 50px;
+  font-size: 20px;
+  overflow: hidden;
+  /* font-size: calc(20px + (16 - 20) * ((100vw - 500px) / (1920 - 500))); */
+  /* padding-bottom: 7px; */
+  margin: 7px 0;
+  /* padding-bottom: calc(7px + (2.5 - 7) * ((100vw - 500px) / (1920 - 500))); */
 }
 
 .place-address {
-  font-size: calc(16px + (12 - 16) * ((100vw - 500px) / (1920 - 500)));
-  padding-bottom: calc(7px + (2.5 - 7) * ((100vw - 500px) / (1920 - 500)));
+  height: 20px;
+  font-size: 16px;
+  overflow: hidden;
+  /* font-size: calc(16px + (12 - 16) * ((100vw - 500px) / (1920 - 500))); */
+  /* padding-bottom: 7px; */
+  margin: 7px 0;
+  /* padding-bottom: calc(7px + (2.5 - 7) * ((100vw - 500px) / (1920 - 500))); */
 }
 
 .button {
   width: 100%;
+  height: 40px;
+  padding-top: 15px;
   border-radius: 5px;
-  margin: calc(7px + (2.5 - 7) * ((100vw - 500px) / (1920 - 500))) 0;
-  padding: calc(10px + (8 - 10) * ((100vw - 500px) / (1920 - 500))) 0;
-  font-size: calc(18px + (16 - 18) * ((100vw - 500px) / (1920 - 500)));
+  /* margin: 7px 0px; */
+  /* margin: calc(7px + (2.5 - 7) * ((100vw - 500px) / (1920 - 500))) 0; */
+  /* padding: 10px 0px; */
+  /* padding: calc(10px + (8 - 10) * ((100vw - 500px) / (1920 - 500))) 0; */
+  font-size: 18px;
+  /* font-size: calc(18px + (16 - 18) * ((100vw - 500px) / (1920 - 500))); */
   color: #fff;
-  text-align: center;
   font-weight: lighter;
   background: #2b3f8d;
 }
