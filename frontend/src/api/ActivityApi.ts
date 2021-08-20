dabf4c4d4636e6ade8771a6656cdd234b25b13f4
Sergei Koroblev <rassafel@gmail.com>
2021-08-21 03:55:00 +0400

    Updated api.

--- frontend/src/api/ActivityApi.ts
+++ frontend/src/api/ActivityApi.ts
@@ -1,10 +1,12 @@
 import axiosApi, { AxiosResponse } from "@/api/AxiosApi";
 
-import { plainToClass } from 'class-transformer';
+import { plainToClass } from "class-transformer";
 
 import { Page } from "@/model/Page";
 import { Tag } from "@/model/Tag";
 import { Activity } from "@/model/Activity";
+import { ActivityStatus } from "@/model/ActivityStatus";
+import { ScheduledActivity } from "@/model/ScheduledActivity";
 
 const activityUrl = "/activities";
 
@@ -14,31 +16,7 @@ const activityApi = {
   },
 
   castResponses(data: Activity[]): Activity[] {
-    return data.map(val => this.castResponse(val));
-  },
-
-  getActivityById(id: number): Promise<AxiosResponse<Activity>> {
-    return axiosApi.get(`${activityUrl}/${id}`);
-  },
-
-  findActivityInfoById(id: number): Promise<AxiosResponse<Activity>> {
-    return axiosApi.get(`${activityUrl}/${id}/info`);
-  },
-
-  getActivitiesByIds(ids: number[]): Promise<AxiosResponse<Activity[]>> {
-    return axiosApi.post(`${activityUrl}/ids`, ids);
-  },
-
-  findActivitiesByName(
-    name: string,
-    size: number,
-    page: number
-  ): Promise<AxiosResponse<Page<Activity>>> {
-    const params = new URLSearchParams();
-    params.set("name", name);
-    params.set("size", size.toString());
-    params.set("page", page.toString());
-    return axiosApi.get(`${activityUrl}/search?${params.toString()}`);
+    return data.map((val) => this.castResponse(val));
   },
 
   findActivitiesByTags(
@@ -46,7 +24,7 @@ const activityApi = {
     size: number,
     page: number
   ): Promise<AxiosResponse<Page<Activity>>> {
-    let tagIds = tags.map((tag) => tag.id);
+    const tagIds = tags.map((tag) => tag.id);
     return this.findActivitiesByTagsIds(tagIds, size, page);
   },
 
@@ -62,6 +40,25 @@ const activityApi = {
     return axiosApi.post(`${activityUrl}?${params.toString()}`, tags);
   },
 
+  getActivities(
+    size: number,
+    page: number
+  ): Promise<AxiosResponse<Page<Activity>>> {
+    return this.findActivitiesByTagsIds([], size, page);
+  },
+
+  findActivitiesByName(
+    name: string,
+    size: number,
+    page: number
+  ): Promise<AxiosResponse<Page<Activity>>> {
+    const params = new URLSearchParams();
+    params.set("name", name);
+    params.set("size", size.toString());
+    params.set("page", page.toString());
+    return axiosApi.get(`${activityUrl}/search?${params.toString()}`);
+  },
+
   findActivitiesByPlaceId(
     placeId: number,
     size: number,
@@ -70,32 +67,51 @@ const activityApi = {
     const params = new URLSearchParams();
     params.set("size", size.toString());
     params.set("page", page.toString());
-    // FIXME: create query
-    return axiosApi.get(`${activityUrl}/place/${placeId}?${params.toString}`)
+    return axiosApi.get(`${activityUrl}/place/${placeId}?${params.toString}`);
+  },
+
+  findActivityById(id: number): Promise<AxiosResponse<Activity>> {
+    return axiosApi.get(`${activityUrl}/${id}`);
+  },
+
+  findActivityInfoById(id: number): Promise<AxiosResponse<Activity>> {
+    return axiosApi.get(`${activityUrl}/${id}/info`);
   },
 
   findScheduledActivitiesByActivityId(
     activityId: number,
     size: number,
-    page: number
-  ) {
-    // TODO: create query
-    return;
+    page: number,
+    statuses: ActivityStatus[] = []
+  ): Promise<AxiosResponse<ScheduledActivity>> {
+    const params = new URLSearchParams();
+    params.set("size", size.toString());
+    params.set("page", page.toString());
+    return axiosApi.post(
+      `${activityUrl}/${activityId}/scheduled?${params.toString}`,
+      statuses
+    );
   },
 
   findActivitiesByDateTime(
     datetime: Date,
     size: number,
-    page: number
-  ) {
+    page: number,
+    statuses: ActivityStatus[] = []
+  ): Promise<AxiosResponse<ScheduledActivity>> {
     const params = new URLSearchParams();
-    params.set("from", datetime.toISOString())
+    params.set("from", datetime.toISOString());
     params.set("size", size.toString());
     params.set("page", page.toString());
-    // TODO: create query
-    return;
-  }
+    return axiosApi.post(
+      `${activityUrl}/search/date?${params.toString}`,
+      statuses
+    );
+  },
+
+  findActivitiesByIds(ids: number[]): Promise<AxiosResponse<Activity[]>> {
+    return axiosApi.post(`${activityUrl}/ids`, ids);
+  },
 };
 
 export default activityApi;
-export { Tag, Activity, Page };
